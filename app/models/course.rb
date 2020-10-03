795d97c66d66ee0f12581dddb7fb3874625cbb76
emir91 <vranacemir@gmail.com>
2020-10-03 19:24:58 +0200

    add students resource, add validations for both resources and display courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  validates :short_name, presence: true, length:{minimum: 5, maximum: 50}
+  validates :name, presence: true, length:{minimum: 3, maximum: 15}
+  validates :desciption, presence: true, length:{minimum: 10, maximum: 300}
 end
