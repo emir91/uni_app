57e5ad32be2d6b48960e42a48b476a8263ddc524
emir91 <vranacemir@gmail.com>
2020-10-10 12:03:13 +0200

    add login routes, create logins controller and new action and create login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'courses#index'
   resources :students, except: [:destroy]
+  get 'login',to:'logins#new'
+  post 'login',to:'logins#create'
+  delete 'logout',to:'logins#destroy'
 end
