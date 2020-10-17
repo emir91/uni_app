a6ed4a30ca57f8d4938a5554c03ce4eaa0a70f61
emir91 <vranacemir@gmail.com>
2020-10-17 13:13:43 +0200

    add font to page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :require_user
 
   def home
-    redirect_to courses_path if logged_in?
     render 'pages/home.html.erb'
+    redirect_to courses_path if logged_in? and return
   end
 
 end
