f9c51d495ff3aa00d10e752272834ae397f54500
emir91 <vranacemir@gmail.com>
2020-10-17 12:05:10 +0200

    add courses to db

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   validates :short_name, presence: true, length:{minimum: 5, maximum: 50}
-  validates :name, presence: true, length:{minimum: 3, maximum: 15}
-  validates :desciption, presence: true, length:{minimum: 10, maximum: 300}
+  validates :name, presence: true, length:{minimum: 3, maximum: 30}
+  validates :description, presence: true, length:{minimum: 10, maximum: 300}
 
   has_many :student_courses
   has_many :students, through: :student_courses
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create(short_name: "CS113", name: "Computer Networks", 
+description: "An introductory look at computer networks and their usage")
+Course.create(short_name: "CS114", name: "Programming 1", 
+description: "Intro to programming using Phyton")
+Course.create(short_name: "CS115", name: "Digital Economy", 
+description: "The course aims to introduce students to the principles of the digital economy")
+
+
